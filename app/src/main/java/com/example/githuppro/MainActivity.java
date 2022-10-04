fef32955b1c1134033457bedd583c232c7f1aaf8
umair4306 <mumair4306@gmail.com>
2022-10-04 12:49:28 +0500

    First Commit

--- app/src/main/java/com/example/githuppro/MainActivity.java
+++ app/src/main/java/com/example/githuppro/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.githuppro;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show();
 
 
     }
